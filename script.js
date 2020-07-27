8ffaed71fcf981e428d1be90da9fffc57f67d548
Adam Madueno <amadueno17@yahoo.com>
2020-07-27 00:29:29 -0700

    added to html, made button work in Java

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+let questions = [
+    question1 = `What is the first letter in the Alphabet?`
+
+
+
+
+
+]
+
+// let answers = {
+//     answer1 = [`A.b`,`B.c`, `C.a`, `D.e`]
+    
+
+// }
+
+document.getElementById('start').addEventListener('click' , event =>{
+    event.preventDefault()
+    // console.log('hello')
+    document.getElementById('Answers').innerHTML = `
+    ${questions.question1}
+    `
+    })
+
+    
+    
+    
+// })
+
+// function addQuestion () {
+//     document.getElementById('Question').innerHTML = `
+//     ${questions.question1}
+//     `
+//  console.log(addQuestion)
+
+// }
+
+
+
+// document.getElementById('Questions').innerHTML () 
+
+
+
+
+//     `${questions.question1}`
+//     console.log(questions.question1)
+
+
+        
